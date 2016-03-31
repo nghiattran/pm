60f81977f4d7dd079b328a0eee9ba3260500016e
NghiaTTran <nghiattran3@gmail.com>
2016-03-30 22:36:02 -0400

    done #6

--- tmp/main.js
+++ tmp/main.js
@@ -12,18 +12,21 @@ function main() {
     // Get required pkgs
     // getDep.getAllDependencies(function (err, res) {
     //   const pkgs = res
-    //   const conflictPkgs = tree.getConflictPkgs(pkgs)
-    //   structurePkgs(pkgs, 'node_modules')
-    //   var promises = []
-    //   for (var i = 0; i < pkgs.length; i++) {
-    //     promises.push(download(pkgs[i]))
-    //   }
-    //   bluebird.all(promises).then(function() {
-    //     console.log("all the files were created");
-    //   })
+    //   // const reducedList = _.uniqWith(pkgs, isSame)
+    //   // const conflictPkgs = tree.getConflictPkgs(pkgs)
+    //   // structurePkgs(pkgs, 'node_modules')
+    //   // console.log(pkgs.length)
+    //   // var promises = []
+    //   // for (var i = 0; i < pkgs.length; i++) {
+    //   //   promises.push(download(pkgs[i]))
+    //   // }
+    //   // bluebird.all(promises).then(function() {
+    //   //   console.log("all the files were created");
+    //   // })
     // })
     // endash.getList(function (err, res) {
     //   if(!err) {
+    //     const reducedList = _.uniqWith(pkgs, isSame)
     //     const pkgs = res
     //     const conflictPkgs = tree.getConflictPkgs(pkgs)
     //     console.log(pkgs)
@@ -40,32 +43,111 @@ function main() {
     //     console.log('err', err)
     //   }
     // })
-    displayTree();
+    displayTree('request', '2.69.0');
+    // displayTree()
 }
-function displayTree() {
-    var treeSymbols = {
-        first: '└─',
-        second: '│ ',
-        third: '┬ ',
-        fourth: '├─'
-    };
+function displayTree(pkgName, version) {
+    if (pkgName === void 0) { pkgName = ''; }
+    if (version === void 0) { version = ''; }
     endash.getList(function (err, res) {
         if (err) {
-            console.log('err', err);
+            // TODO chang file's name
+            throw "Can't find json file";
         }
         else {
             var pkgs = res;
             var list = tree.makeTree(pkgs);
-            // console.log(list['npm-openweathermap'].dependencies)
-            var tmp = list['npm-openweathermap'].dependencies;
-            for (var i = 0; i < tmp.length; i++) {
-                console.log(tmp[i]);
+            if (pkgName) {
+                var foundPkgs = findPkgName(pkgs, pkgName, version);
+                for (var i = 0; i < foundPkgs.length; i++) {
+                    var tmp = {};
+                    tmp[foundPkgs[i].name] = foundPkgs[i];
+                    treeLine(tmp);
+                }
+            }
+            else {
+                treeLine(list);
             }
         }
     });
 }
+/**
+ * [findPkgName description]
+ * Look for a particular pkg in the array
+ */
+function findPkgName(pkgs, pkgName, version) {
+    if (version === void 0) { version = ''; }
+    var samePkg = [];
+    var res = [];
+    for (var i = 0; i < pkgs.length; i++) {
+        if (pkgs[i].name === pkgName) {
+            var pkgInfo = {
+                name: pkgs[i].name,
+                version: pkgs[i].version
+            };
+            if (_.findIndex(samePkg, pkgInfo) === -1) {
+                samePkg.push(pkgInfo);
+                res.push(i);
+            }
+        }
+    }
+    if (version) {
+        var index = [_.findIndex(samePkg, {
+                name: pkgName,
+                version: version
+            })];
+        return endash.copy(pkgs, [res[index]]);
+    }
+    return endash.copy(pkgs, res);
+}
+var treeSymbols = {
+    first: '└─',
+    second: '│ ',
+    third: '┬ ',
+    fourth: '├─',
+    fifth: '─ '
+};
+function treeLine(tree, line) {
+    if (line === void 0) { line = ''; }
+    var tempLine = line;
+    var space = '';
+    var treeLength = Object.keys(tree).length;
+    var i = 0;
+    for (var pkg in tree) {
+        var pkgInfo = tree[pkg].name + '@' + tree[pkg].version;
+        // If the pkg is the only one or is the last one
+        // Display with '└─name'
+        // else
+        // Display with '├─'
+        if (treeLength === 1 || i === treeLength - 1) {
+            tempLine += treeSymbols.first;
+            space = '  ';
+        }
+        else {
+            tempLine += treeSymbols.fourth;
+            space = treeSymbols.second;
+        }
+        // If the pkg does not have dependencies
+        // Display with '└─name'
+        // else
+        // Go to the next layer with prefix 'space'
+        if (_.isEmpty(tree[pkg].dependencies)) {
+            console.log(tempLine + treeSymbols.fifth + pkgInfo);
+        }
+        else {
+            console.log(tempLine + treeSymbols.third + pkgInfo);
+            treeLine(tree[pkg].dependencies, line + space);
+        }
+        tempLine = line;
+        i++;
+    }
+}
+/**
+ * Structure npm pkgs
+ * Conflict pkgs are put in to node_modules in their dependent pkg
+ * Non coflict pkgs are put int root node_modules
+ */
 function structurePkgs(pkgs, storeFolder) {
-    console.log(pkgs.length);
     for (var i = 0; i < pkgs.length; i++) {
         if ('isConflict' in pkgs[i]) {
             var pathArray = pkgs[i].path.split(path.sep);
--- ts/components/endash.ts
+++ ts/components/endash.ts
@@ -54,9 +54,67 @@ export function set(object: Object, path: any[], value: any): Object {
 
     if (typeof object[tmp] !== 'object') {
       return {
-        err: 'Error! Expect ' + tmp + 'to be an object. Get ' + typeof object[tmp] + ' instead.'
+        err: 'Error! Expect ' + tmp + ' to be an object. Get ' + typeof object[tmp] + ' instead.'
       }
     }
     return set(object[tmp], path, value)
   }
+}
+
+
+export function get(object: Object, path: any[]): Object {
+  if (typeof object !== 'object') {
+    return {
+      err: 'Error! Expect an object.'
+    }
+  }
+
+  if (path.length === 1) {
+    return object[path[0]]
+  } else if (path.length > 1) {
+    var tmp = path.shift()
+
+    if (typeof object[tmp] !== 'object') {
+      return {
+        err: 'Error! Expect ' + tmp + ' to be an object. Get ' + typeof object[tmp] + ' instead.'
+      }
+    }
+    return get(object[tmp], path)
+  }
+}
+
+/**
+ * [isSame description]
+ * Is used to check wether 2 pkgs are the similar
+ */
+export function isSame(left, right) : Boolean {
+  if (left.name === right.name)
+  {
+    if (left.version !== right.version) {
+      return false
+    }
+    return true
+  }
+  return false
+}
+
+/**
+ * Map 2 array: 1 object array and index array
+ * Example:
+ * arr1: ['first', 'second', 'third']
+ * arr2: [3,2]
+ *
+ * return: ['third', 'second']
+ */
+export function copy(arr1, arr2) {
+  let res = []
+  for (var i = 0; i < arr2.length; i++) {
+    if (arr2[i] < 0 || arr2[i] > arr1.length) {
+      throw 'Out of range.'
+    }
+
+    res.push(arr1[arr2[i]])
+  }
+
+  return res
 }
