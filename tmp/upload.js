ff52d52ac77e3329012119390f1a8dccc162fe57
NghiaTTran <nghiattran3@gmail.com>
2016-03-31 15:44:46 -0400

    done #5: ignore files in .ignore

--- tmp/upload.js
+++ tmp/upload.js
@@ -4,51 +4,77 @@ var zlib = require('zlib');
 var tar = require('tar-fs');
 var path = require('path');
 var request = require('request');
-var rimraf = require('rimraf');
-var file = __dirname;
+var UPLOAD_URL = 'http://localhost:9000/package';
+var BASE_DIR = __dirname;
 var tempDir = '.tmp';
 var tempGzipFile = 'tmp.tgz';
-function upload(toPath) {
-    // console.log(toPath)
-    var json = fs.createReadStream(path.join(toPath, 'index.js'));
-    compress(toPath, function (toPath) {
-        // console.log(toPath)
+/**
+ * fromPath: path to base pkg
+ */
+function upload(fromPath) {
+    // TODO: This should a json file
+    var json = fs.createReadStream(path.join(fromPath, 'index.js'));
+    compress(fromPath, function (toPath) {
         var pkg = fs.createReadStream(path.join(toPath, tempGzipFile));
         var formData = {
             pkg: pkg,
             json: json
         };
-        doUpload(formData);
-        rimraf(toPath, function (err) {
-            if (err)
-                throw err;
-            console.log('successfully deleted ' + toPath);
-        });
+        // doUpload(formData)
+        // rimraf(toPath, (err) => {
+        //   if (err) throw err;
+        //   console.log('successfully deleted ' + toPath);
+        // });
     });
 }
-function compress(toPath, cb) {
-    var temp = path.join(toPath, tempDir);
-    var tempFile = path.join(temp, tempGzipFile);
-    if (!fs.existsSync(temp)) {
-        fs.mkdirSync(temp);
+function compress(fromPath, cb) {
+    var toPath = path.join(fromPath, tempDir);
+    var tempFile = path.join(toPath, tempGzipFile);
+    var ignoreFiles = readIgnoreFile(fromPath);
+    if (!fs.existsSync(toPath)) {
+        fs.mkdirSync(toPath);
     }
-    tar.pack(toPath)
-        .pipe(zlib.createGzip())
+    // TODO: ignore globs
+    var stream = tar.pack(fromPath, { ignore: function (name) {
+            for (var i = 0; i < ignoreFiles.length; i++) {
+                if (path.join(fromPath, ignoreFiles[i]) === name) {
+                    return true;
+                }
+            }
+            return false;
+        } });
+    stream.pipe(zlib.createGzip())
         .pipe(fs.createWriteStream(tempFile))
         .on('finish', function () {
-        console.log('end');
-        cb(temp);
+        console.log(tempFile);
+        cb(toPath);
     });
 }
+/**
+ * Read in .pkgignore from root directory
+ */
+function readIgnoreFile(fromPath) {
+    try {
+        var content = fs.readFileSync(path.join(fromPath, '.pkgignore'), 'utf8');
+        var array = content.split('\n');
+        return array;
+    }
+    catch (e) {
+        return e;
+    }
+}
+/**
+ * Perfom upload
+ */
 function doUpload(formData) {
     var headers = {
         'Content-encoding': 'gzip'
     };
-    return request.post({ url: 'http://localhost:9000/package', formData: formData, headers: headers }, function optionalCallback(err, httpResponse, body) {
+    return request.post({ url: UPLOAD_URL, formData: formData, headers: headers }, function optionalCallback(err, httpResponse, body) {
         if (err) {
             return console.error('upload failed:', err);
         }
         console.log('Upload successful!  Server responded with:', body);
     });
 }
-upload(file);
+upload(BASE_DIR);
--- ts/upload.ts
+++ ts/upload.ts
@@ -6,50 +6,86 @@ import * as path from 'path'
 import * as request from 'request'
 import * as readdirp from 'readdirp'
 import * as rimraf from 'rimraf'
+import * as isGlob from 'isGlob'
 
-const file = __dirname
+const UPLOAD_URL = 'http://localhost:9000/package'
+
+const BASE_DIR = __dirname
 const tempDir = '.tmp'
 const tempGzipFile = 'tmp.tgz'
 
-function upload(toPath) {
-  // console.log(toPath)
-  const json = fs.createReadStream(path.join(toPath, 'index.js'))
-  compress(toPath, function(toPath) {
-    // console.log(toPath)
+/**
+ * fromPath: path to base pkg
+ */
+
+function upload(fromPath) {
+  // TODO: This should a json file
+  const json = fs.createReadStream(path.join(fromPath, 'index.js'))
+  compress(fromPath, function(toPath) {
     const pkg = fs.createReadStream(path.join(toPath, tempGzipFile))
     const formData = {
       pkg,
       json
     }
-    doUpload(formData)
-    rimraf(toPath, (err) => {
-      if (err) throw err;
-      console.log('successfully deleted ' + toPath);
-    });
+    // doUpload(formData)
+    // rimraf(toPath, (err) => {
+    //   if (err) throw err;
+    //   console.log('successfully deleted ' + toPath);
+    // });
   })
 }
 
-function compress(toPath, cb) {
-  const temp = path.join(toPath, tempDir)
-  const tempFile = path.join(temp, tempGzipFile)
-  if (!fs.existsSync(temp) ){
-    fs.mkdirSync(temp);
+function compress(fromPath, cb) {
+  const toPath = path.join(fromPath, tempDir)
+  const tempFile = path.join(toPath, tempGzipFile)
+  const ignoreFiles = readIgnoreFile(fromPath)
+
+  if (!fs.existsSync(toPath)){
+    fs.mkdirSync(toPath);
   }
-  tar.pack(toPath)
-    .pipe(zlib.createGzip())
+
+  // TODO: ignore globs
+  let stream = tar.pack(fromPath, { ignore: function (name) {
+    for (var i = 0; i < ignoreFiles.length; i++) {
+      if (path.join(fromPath, ignoreFiles[i]) === name) {
+        return true
+      }
+    }
+
+    return false
+  }})
+    
+  stream.pipe(zlib.createGzip())
     .pipe(fs.createWriteStream(tempFile))
     .on('finish', function () {
-      console.log('end')
-      cb(temp)
+      console.log(tempFile)
+      cb(toPath)
     })
 }
 
+/**
+ * Read in .pkgignore from root directory
+ */
+function readIgnoreFile(fromPath) {
+  try {
+    const content = fs.readFileSync(path.join(fromPath,'.pkgignore'), 'utf8')
+    const array = content.split('\n')
+    return array
+  } catch (e) {
+    return e
+  }
+}
+
+
+/**
+ * Perfom upload
+ */
 function doUpload (formData) {
   var headers = {
     'Content-encoding': 'gzip'
   }
 
-  return request.post({url:'http://localhost:9000/package', formData:formData, headers:headers}, function optionalCallback(err, httpResponse, body) {
+  return request.post({url: UPLOAD_URL, formData:formData, headers:headers}, function optionalCallback(err, httpResponse, body) {
     if (err) {
       return console.error('upload failed:', err);
     }
@@ -58,4 +94,4 @@ function doUpload (formData) {
 }
 
 
-upload(file)
+upload(BASE_DIR)
